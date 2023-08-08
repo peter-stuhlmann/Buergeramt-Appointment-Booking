2ccbe67e4da61abc1ae5f52ef352c4f1d06e1154
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2023-08-08 23:05:45 +0200

    Add start_day arg to specify a day for the earliest appointment

--- booking.py
+++ booking.py
@@ -3,11 +3,13 @@ import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import datetime
 
 parser = argparse.ArgumentParser(description="This script will book you the next available appointment at a Bürgeramt (citizen's office) in Berlin.")
 parser.add_argument("--name", required=True, help="Your name")
 parser.add_argument("--email", required=True, help="Your email address")
 parser.add_argument("--id", required=True, help="Service ID")
+parser.add_argument("--start_day", type=int, default=datetime.datetime.now().day, help="Start day for booking appointments (default: current day)")
 args = parser.parse_args()
 
 your_name = args.name
@@ -53,6 +55,19 @@ def click_first_link(browser, class_name):
     except:
         return False
 
+def get_next_available_day(browser, start_day):
+    available_days = browser.find_elements(By.XPATH, "//td[contains(@class, 'buchbar')]//a")
+    today = datetime.datetime.now().day
+
+    for day_element in available_days:
+        day_text = day_element.text
+        day = int(day_text)
+        
+        if today <= start_day and day >= start_day:
+            return day_element
+
+    return None
+
 def fill_and_submit_form(browser):
     family_name_input = browser.find_element(By.ID, "familyName")
     family_name_input.send_keys(your_name)
@@ -69,6 +84,7 @@ def fill_and_submit_form(browser):
     browser.execute_script("arguments[0].click();", register_submit_button)
 
 def main():
+    start_day = args.start_day
     while True:
         print("Script is running...")
         result, browser = run_script()
@@ -76,8 +92,10 @@ def main():
             print("No available dates, try again...")
         else:
             print("Available appointment found.")
-            if click_first_link(browser, "buchbar"):
-                print("Date selected.")
+            next_available_day = get_next_available_day(browser, start_day)
+            if next_available_day:
+                next_available_day.click()
+                print(f"Date {start_day} or later selected.")
                 if click_first_link(browser, "frei"):
                     print("Time selected.")
                     fill_and_submit_form(browser)
@@ -87,7 +105,7 @@ def main():
                 else:
                     print("The free appointment is no longer available. A new attempt will be started.")
             else:
-                print("The free appointment is no longer available. A new attempt will be started.")
+                print("No available appointment from the specified start day. A new attempt will be started.")
 
 if __name__ == "__main__":
     main()
